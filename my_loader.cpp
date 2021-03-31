b376c68fedc3a13d577a042c367588509bd4ee12
megumin_h <61540441+hungrymegumin@users.noreply.github.com>
2021-03-31 22:58:01 +0800

    rename some structs and functions, fix memory leak

--- my_loader.cpp
+++ my_loader.cpp
@@ -22,34 +22,34 @@
 using namespace std;
 
 //type item
-typedef struct WASMType {
-	int  param_num;
-	int  return_num;
+typedef struct DEEPType {
+	int  param_count;
+	int  ret_count;
 	char type[1];
-} WASMType;
+} DEEPType;
 
 //local variables item
 typedef struct LocalVars {
-	int   num;
+	int   count;
 	short local_type;
 } LocalVars;
 
 //function item
-typedef struct WASMFunction {
-	WASMType*   func_type; // the type of function
+typedef struct DEEPFunction {
+	DEEPType*   func_type; // the type of function
 	LocalVars** localvars;
 	int         code_size;
 	char*       code_begin;
-} WASMFunction;
+} DEEPFunction;
 
 /* Data structure of module, at present we only support 
 two sections, which can make the program run*/
-typedef struct WASMModule {
+typedef struct DEEPModule {
 	int            type_count;
 	int            function_count;
-	WASMType**     type_section;
-	WASMFunction** func_section;
-} WASMModule;
+	DEEPType**     type_section;
+	DEEPFunction** func_section;
+} DEEPModule;
 
 //the difinition of listnode
 typedef struct section_listnode {
@@ -60,10 +60,10 @@ typedef struct section_listnode {
 } section_listnode;
 
 //read a value of specified type
-#define TEMPLATE_READ_VALUE(Type, p) \
+#define READ_VALUE(Type, p) \
 	(p += sizeof(Type), *(Type*)(p - sizeof(Type)))
-#define READ_INT(p)  TEMPLATE_READ_VALUE(int, p)
-#define READ_CHAR(p) TEMPLATE_READ_VALUE(char, p)
+#define READ_INT(p)  READ_VALUE(int, p)
+#define READ_CHAR(p) READ_VALUE(char, p)
 
 //解码一个无符号32位或64整型，目前只会用到32位类型,在大多数实现中占5个字节
 static int read_leb_u32(char** p) {
@@ -108,67 +108,65 @@ static bool create_section_list(const char** p, int size, section_listnode* sect
 }
 
 //读取类型段
-static void load_type_section(const char* p, WASMModule* module) {
+static void decode_type_section(const char* p, DEEPModule* module) {
 
 	const char* p_tmp;
 	int         total_size = 0;
-	int         type_count = 0, param_num = 0, return_num = 0;
+	int         type_count = 0, param_count = 0, ret_count = 0;
 	module->type_count = type_count = read_leb_u32((char**)&p);
-	module->type_section            = (WASMType**)malloc(type_count * sizeof(WASMType*));
+	module->type_section            = (DEEPType**)malloc(type_count * sizeof(DEEPType*));
 	for (int i = 0; i < type_count; i++) {
 		if (READ_CHAR(p) == 0x60) {
-			param_num = read_leb_u32((char**)&p);
+			param_count = read_leb_u32((char**)&p);
 			p_tmp     = p;
-			p += param_num;
-			return_num = read_leb_u32((char**)&p);
+			p += param_count;
+			ret_count = read_leb_u32((char**)&p);
 			p          = p_tmp;
-			total_size = 8 + param_num + return_num;
+			total_size = 8 + param_count + ret_count;
 
-			module->type_section[i]             = (WASMType*)malloc(total_size);
-			module->type_section[i]->param_num  = param_num;
-			module->type_section[i]->return_num = return_num;
+			module->type_section[i]             = (DEEPType*)malloc(total_size);
+			module->type_section[i]->param_count  = param_count;
+			module->type_section[i]->ret_count = ret_count;
 
-			for (int j = 0; j < param_num; j++) {
+			for (int j = 0; j < param_count; j++) {
 				module->type_section[i]->type[j] = READ_CHAR(p);
 			}
 			read_leb_u32((char**)&p);
-			for (int j = 0; j < return_num; j++) {
-				module->type_section[i]->type[param_num + j] = READ_CHAR(p);
+			for (int j = 0; j < ret_count; j++) {
+				module->type_section[i]->type[param_count + j] = READ_CHAR(p);
 			}
 		}
 	}
 }
 
 //读取函数段
-static void load_func_section(const char* p, WASMModule* module, const char* p_code) {
+static void decode_func_section(const char* p, DEEPModule* module, const char* p_code) {
 	int           func_count      = read_leb_u32((char**)&p);
 	int           code_func_count = read_leb_u32((char**)&p_code);
 	int           type_index, code_size, local_set_count;
-	WASMFunction* func;
+	DEEPFunction* func;
 	LocalVars*    local_set;
 	const char*   p_code_temp;
 	if (func_count == code_func_count) {
 		module->function_count = func_count;
-		module->func_section   = (WASMFunction**)malloc(func_count * sizeof(WASMFunction*));
+		module->func_section   = (DEEPFunction**)malloc(func_count * sizeof(DEEPFunction*));
 
 		for (int i = 0; i < func_count; i++) {
-			func = module->func_section[i] = (WASMFunction*)malloc(sizeof(WASMFunction));
-			memset(func, 0, sizeof(WASMFunction));
+			func = module->func_section[i] = (DEEPFunction*)malloc(sizeof(DEEPFunction));
+			memset(func, 0, sizeof(DEEPFunction));
 			type_index  = read_leb_u32((char**)&p);
 			code_size   = read_leb_u32((char**)&p_code);
 			p_code_temp = p_code;
-			cout << code_size << endl;
 			func->func_type = module->type_section[type_index];
 			func->code_size = code_size;
 			local_set_count = read_leb_u32((char**)&p_code);
-			cout << local_set_count << endl;
 			if (local_set_count == 0) {
 				func->localvars = NULL;
 			} else {
 				func->localvars = (LocalVars**)malloc(local_set_count * sizeof(LocalVars*));
 				for (int j = 0; j < local_set_count; j++) {
 					local_set = func->localvars[j] = (LocalVars*)malloc(sizeof(LocalVars));
-					local_set->num                 = read_leb_u32((char**)&p_code);
+					local_set->count                 = read_leb_u32((char**)&p_code);
 					local_set->local_type          = READ_CHAR(p_code);
 				}
 			}
@@ -179,7 +177,7 @@ static void load_func_section(const char* p, WASMModule* module, const char* p_c
 }
 
 //依次遍历每个section，遇到需要的就load进module
-static void load_from_sections(WASMModule* module, section_listnode* section_list) {
+static void decode_each_sections(DEEPModule* module, section_listnode* section_list) {
 
 	section_listnode* section = section_list;
 	const char *      buf = NULL, *p_code = NULL;
@@ -194,7 +192,7 @@ static void load_from_sections(WASMModule* module, section_listnode* section_lis
 
 			break;
 		case SECTION_TYPE_TYPE:
-			load_type_section(buf, module);
+			decode_type_section(buf, module);
 
 			break;
 		case SECTION_TYPE_IMPORT:
@@ -202,7 +200,7 @@ static void load_from_sections(WASMModule* module, section_listnode* section_lis
 			break;
 		case SECTION_TYPE_FUNC:
 
-			load_func_section(buf, module, p_code);
+			decode_func_section(buf, module, p_code);
 
 			break;
 		case SECTION_TYPE_TABLE:
@@ -238,7 +236,7 @@ static void load_from_sections(WASMModule* module, section_listnode* section_lis
 }
 
 //load
-static WASMModule* load(char** p, int size) {
+static DEEPModule* deep_load(char** p, int size) {
 	if (!check_magic_number_and_version(p))
 		return NULL;
 	section_listnode* section_list = (section_listnode*)malloc(sizeof(section_list));
@@ -246,13 +244,20 @@ static WASMModule* load(char** p, int size) {
 	size -= 8;
 	create_section_list((const char**)p, size, section_list);
 
-	WASMModule* module = (WASMModule*)malloc(sizeof(WASMModule));
+	DEEPModule* module = (DEEPModule*)malloc(sizeof(DEEPModule));
 	memset(module, 0, sizeof(module));
-	load_from_sections(module, section_list->next);
+	decode_each_sections(module, section_list->next);
 
-	free(section_list); //链表的free要自己实现，目前还未实现
+	section_listnode* dummy = section_list, *q;
+	while(dummy != NULL) {
+		q = dummy->next;
+		free(dummy);
+		dummy = q;
+	}
+	section_list = NULL;
 	return module;
 }
+
 
 int main() {
 	const char* path = "program.wasm";
@@ -264,5 +269,5 @@ int main() {
 		cout << "open fail" << endl;
 	}
 	size = (int)read(file, p, 1024);
-	WASMModule* module = load(&p, size);
+	DEEPModule* module = deep_load(&p, size);
 }
