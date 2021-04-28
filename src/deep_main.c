d3e0ffdd605f8c840ad0edf678173f3e0ed0c270
xj <3294013584@qq.com>
2021-04-28 22:25:50 +0800

    fix bug

--- src/deep_main.c
+++ src/deep_main.c
@@ -21,7 +21,6 @@ int32_t main(int argv, char ** args) {
         return -1;
     }
 
-    path = "/home/xj/deepvm_developing/example/test.wasm";
     FILE *fp = fopen(path, "rb"); /* read wasm file with binary mode */
     if(fp == NULL) {
         printf("file open fail.\r\n");
