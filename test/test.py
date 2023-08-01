6afc27a829363e44b20adbf13fb13c585ef3da12
hobbitqia <861394755@qq.com>
2023-08-01 20:58:45 +0800

    添加了 control 的测试用例

--- test/test.py
+++ test/test.py
@@ -80,6 +80,33 @@ test_with_path('control/switch_case_001.wasm', '65')
 test_with_path('control/tri_if_001.wasm', '30')
 test_with_path('control/tri_if_002.wasm', '60')
 
+test_with_path('control/if/if_32_1.wasm', 100)
+test_with_path('control/if/if_64_1.wasm', 100)
+test_with_path('control/if/if_32_2.wasm', 50)
+test_with_path('control/if/if_64_2.wasm', 50)
+test_with_path('control/if/if_32_3.wasm', 20)
+test_with_path('control/if/if_64_3.wasm', 20)
+test_with_path('control/if/if_32_4.wasm', 10)
+test_with_path('control/if/if_64_4.wasm', 10)
+test_with_path('control/if/if_32_5.wasm', -1)
+test_with_path('control/if/if_64_5.wasm', -1)
+
+test_with_path('control/loop/for_32_1.wasm', 45)
+test_with_path('control/loop/for_64_1.wasm', 45)
+test_with_path('control/loop/for_32_2.wasm', 450)
+# test_with_path('control/loop/for_64_2.wasm', 50)
+test_with_path('control/loop/for_32_3.wasm', 120)
+test_with_path('control/loop/for_32_4.wasm', 240)
+test_with_path('control/loop/for_32_5.wasm', 1225)
+test_with_path('control/loop/for_32_6.wasm', 112)
+test_with_path('control/loop/for_32_7.wasm', 323401)
+
+test_with_path('control/loop/while_32_1.wasm', 330)
+test_with_path('control/loop/while_64_1.wasm', 330)
+# break continue 
+# switch case
+# jump table
+
 if total_failures > 0:
     print(f"Total {total_failures} tests failed!")
     exit(1)
